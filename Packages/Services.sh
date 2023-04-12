8178031ce0fe07b2d4f2cbeda0af65c6815af139
LeeBRogers <LeeBRogersLR@outlook.com>
2023-04-12 15:26:55 +0100

    Updated services and package lists

--- Packages/Services.sh
+++ Packages/Services.sh
@@ -3,10 +3,10 @@
 systemctl enable NetworkManager
 systemctl enable avahi-daemon
 systemctl enable cups
+systemctl enable earlyoom
 systemctl enable firewalld
 systemctl enable fstrim.timer
 systemctl enable paccache.timer
 systemctl enable power-profiles-daemon # Power management daemon for laptops
 systemctl enable reflector.timer
 systemctl enable sddm
-systemctl enable systemd-oomd

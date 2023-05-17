af39f5c14d216d0e5d50a8dcbb2acb2cafd3d38d
LeeBRogers <LeeBRogersLR@outlook.com>
2023-05-17 16:29:57 +0100

    Updated services

--- Packages/Services.sh
+++ Packages/Services.sh
@@ -10,3 +10,4 @@ systemctl enable paccache.timer
 systemctl enable power-profiles-daemon # Power management daemon for laptops
 systemctl enable reflector.timer
 systemctl enable sddm
+systemctl enable systemd-timesyncd
